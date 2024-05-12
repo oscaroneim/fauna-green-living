1c7509b7c1e4f0af359d079001d67b2db750c343
RehanaKauser <rehana_95@hotmail.co.uk>
2024-05-12 13:18:01 +0100

    completed layout of the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -33,6 +33,33 @@ export default async function About() {
           </p>
         </div>
       </div>
+      <div className="flex h-[664px] w-full gap-x-6 max-md:flex-col">
+        <div className="flex h-full shrink-0 basis-[586px] flex-col gap-y-6">
+          <div className="h-full space-y-4 rounded-lg bg-green p-10 text-center text-white">
+            <h2 className="text-5xl font-bold">FILL</h2>
+            <p className="text-4xl">fill with content</p>
+          </div>
+          <div className=" h-full space-y-4 rounded-lg bg-darkgreen p-10 text-center text-white">
+            <h2 className="text-5xl font-bold">FILL</h2>
+            <p className="text-4xl">fill with content</p>
+          </div>
+        </div>
+        <Image className="rounded-lg object-cover" src={dear} alt="Picture of the dear" />
+      </div>
+      <div className="space-y-8 rounded-lg bg-green px-20 py-16 text-center text-white">
+        <h2 className="text-5xl font-medium">Turning the Tide</h2>
+        <div className="flex text-xl">
+          <h3 className="basis-1/3">
+            <p className="text-4xl">24,356</p> sqm plastic reduced from landfill and ocean
+          </h3>
+          <h3 className="basis-1/3">
+            <p className="text-4xl">10,322</p>single use plastic reduced{' '}
+          </h3>
+          <h3 className="basis-1/3">
+            <p className="text-4xl">100%</p>cotton, plastic free and home compostable
+          </h3>
+        </div>
+      </div>
     </MaxWidthWrapper>
   );
 }
