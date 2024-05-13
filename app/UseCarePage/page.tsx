fad05bd71cb6e3f8100c41be20d6138be53fb8fd
“reehachoudhurygit config --global user.email “reeha.choudhury@gmail.com <reeha.choudhury@gmail.com>
2024-05-13 17:26:30 +0100

    save

--- app/UseCarePage/page.tsx
+++ app/UseCarePage/page.tsx
@@ -1,5 +1,4 @@
-import 'app/UseCarePage/page.css';
-import 'react';
+import './page.css';
 
 export const metadata = {
   title: 'Use & Care',
@@ -10,6 +9,67 @@ export default async function useCarePage() {
   return (
     <>
       <h1>Use & Care</h1>
+      <p>The best care for your item so you can use them over and over. And over, again.</p>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <p className="title">FLIP CARD</p>
+            <p>Hover Me</p>
+          </div>
+          <div className="flip-card-back">
+            <p className="title">BACK</p>
+            <p>Leave Me</p>
+          </div>
+        </div>
+      </div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <p className="title">FLIP CARD</p>
+            <p>Hover Me</p>
+          </div>
+          <div className="flip-card-back">
+            <p className="title">BACK</p>
+            <p>Leave Me</p>
+          </div>
+        </div>
+      </div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <p className="title">FLIP CARD</p>
+            <p>Hover Me</p>
+          </div>
+          <div className="flip-card-back">
+            <p className="title">BACK</p>
+            <p>Leave Me</p>
+          </div>
+        </div>
+      </div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <p className="title">FLIP CARD</p>
+            <p>Hover Me</p>
+          </div>
+          <div className="flip-card-back">
+            <p className="title">BACK</p>
+            <p>Leave Me</p>
+          </div>
+        </div>
+      </div>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <p className="title">FLIP CARD</p>
+            <p>Hover Me</p>
+          </div>
+          <div className="flip-card-back">
+            <p className="title">BACK</p>
+            <p>Leave Me</p>
+          </div>
+        </div>
+      </div>
       <div className="flip-card">
         <div className="flip-card-inner">
           <div className="flip-card-front">
