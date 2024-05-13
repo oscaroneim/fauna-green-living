3fb63e6b83b396e9027a990242f1da5a6ba2d897
donaldliu1 <donaldliu1313@gmail.com>
2024-05-13 16:50:24 +0100

    renamed social to blog, removed bottom of footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -104,21 +104,6 @@ export default async function Footer() {
           <FooterMenu menu={menu} />
         </Suspense>
       </div>
-      <div className="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
-        <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
-          <p>
-            &copy; {copyrightDate} {copyrightName}
-            {copyrightName.length && !copyrightName.endsWith('.') ? '.' : ''} All rights reserved.
-          </p>
-          <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
-          <p>Designed by DORRA Studios</p>
-          <p className="md:ml-auto">
-            <a href="https://vercel.com" className="text-black dark:text-white">
-              Crafted by ▲ Vercel
-            </a>
-          </p>
-        </div>
-      </div>
     </footer>
   );
 }
