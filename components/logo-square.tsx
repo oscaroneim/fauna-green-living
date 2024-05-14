8063647bfb2df18635b125fceeffa9a26278a5ed
donaldliu1 <donaldliu1313@gmail.com>
2024-05-14 13:18:03 +0100

    changes to footer and logo sizes

--- components/logo-square.tsx
+++ components/logo-square.tsx
@@ -7,7 +7,7 @@ export default function LogoSquare({ size }: { size?: 'm' | undefined }) {
       className={clsx(
         'flex flex-none items-center justify-center bg-white dark:border-neutral-700 dark:bg-white',
         {
-          'h-[120px] w-[280px] rounded-xl': !size,
+          'h-[60px] w-[140px] rounded-xl xl:h-[120px] xl:w-[280px]': !size,
           'h-[50px] w-[160px] rounded-lg': size === 'm'
         }
       )}
