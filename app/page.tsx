822dd77a7e097c1ebf37728986fa178764300795
donaldliu1 <donaldliu1313@gmail.com>
2024-05-14 18:02:04 +0100

    converted reviews to component, resuable on other pages, added ticker to homepage and basic styling of product carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+import PlasticWasteStats from 'components/about/plastic-waste-stats';
 import { ThreeItemGrid } from 'components/grid/three-items';
 import Footer from 'components/layout/footer';
 import { Carousel } from 'components/product-carousel';
+import { Reviews } from 'components/reviews';
 
 export const metadata = {
   description: 'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
@@ -13,7 +15,11 @@ export default async function HomePage() {
   return (
     <>
       <ThreeItemGrid />
-      <Carousel />
+      <div className="mx-none h-[635px] bg-[#677B6A] px-2 md:px-0">
+        <Carousel />
+        <Reviews />
+      </div>
+      <PlasticWasteStats />
       <Footer />
     </>
   );
