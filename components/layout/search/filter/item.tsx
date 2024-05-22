bb8eae427897799dcb13c7af58e911af28c0834a
Oscar Oneim <52040766+oscaroneim@users.noreply.github.com>
2024-05-22 11:55:11 +0100

    added hover state to shop

--- components/layout/search/filter/item.tsx
+++ components/layout/search/filter/item.tsx
@@ -17,12 +17,15 @@ function PathFilterItem({ item }: { item: PathFilterItem }) {
   newParams.delete('q');
 
   return (
-    <li className="mt-2 flex text-black" key={item.title}>
+    <li className="mt-2 flex h-auto w-auto text-black" key={item.title}>
       <DynamicTag
         href={createUrl(item.path, newParams)}
-        className={clsx('w-full text-sm underline-offset-4 hover:underline', {
-          'underline underline-offset-4': active
-        })}
+        className={clsx(
+          'w-full text-sm underline-offset-4 md:rounded-md md:px-1 md:hover:bg-customGreen',
+          {
+            'underline underline-offset-4': active
+          }
+        )}
       >
         {item.title}
       </DynamicTag>
