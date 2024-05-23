09dfcc593256072fd3e4de0103579e7cfe346270
RehanaKauser <rehana_95@hotmail.co.uk>
2024-05-23 15:09:13 +0100

    new letter completed just need to verify email

--- components/layout/newsletter-signup.tsx
+++ components/layout/newsletter-signup.tsx
@@ -1,78 +1,72 @@
 'use client';
-
 import emailjs from '@emailjs/browser';
-import React, { useState } from 'react';
+import useAsyncTransition from 'lib/hooks/use-async-transition';
+import { useState, type ChangeEvent, type FC } from 'react';
 import Spinner from '../spinner'; // Adjust the import path based on your project structure
 
-const SignupForm: React.FC = () => {
+const SignupForm: FC = () => {
   const [email, setEmail] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
-  const [isLoading, setIsLoading] = useState(false); // Loading state
-
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const [message, setMessage] = useState('');
+  const [isPending, startTransition] = useAsyncTransition();
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
   };
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    setIsLoading(true); // Set loading state to true when form is submitted
-
-    // EmailJS configuration
-    const serviceID = 'service_5vevvdg';
-    const templateID = 'template_jsz1wbo';
-    const userID = 'LeeUSkEtxK_E8jCVb';
-
-    // EmailJS parameters
-    const templateParams = {
-      user_email: email
-    };
-
-    // Send email via EmailJS
-    emailjs
-      .send(serviceID, templateID, templateParams, userID)
-      .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
-        setSuccessMessage('Signed up for newsletter!');
-        setEmail(''); // Clear the input field after submission
-        setIsLoading(false); // Reset loading state
-        setTimeout(() => {
-          setSuccessMessage(''); // Clear success message after a delay
-        }, 3000); // Adjust the delay as needed
-      })
-      .catch((error) => {
-        console.error('FAILED...', error);
-        setSuccessMessage('There was an error signing up. Please try again.');
-        setIsLoading(false); // Reset loading state
-        setTimeout(() => {
-          setSuccessMessage(''); // Clear error message after a delay
-        }, 2000); // Adjust the delay as needed
-      });
+  const validateEmail = (email: string) =>
+    email
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  const updateMessage = (message: string) => {
+    setMessage(message);
+    setTimeout(() => {
+      setMessage(''); // Clear error message after a delay
+    }, 2000);
   };
-
+  const handleSubmit = () => {
+    startTransition(async () => {
+      try {
+        if (!email || !validateEmail) {
+          updateMessage('Please enter a valid email');
+          return;
+        }
+        await emailjs.send(
+          process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+          process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID_NEWSLETTER!,
+          { email },
+          process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+        );
+        updateMessage('Signed up for newsletter!');
+        setEmail(''); // Clear the input field after submission
+      } catch (e) {
+        console.error('FAILED...', e);
+        updateMessage('There was an error signing up. Please try again.');
+      }
+    });
+  };
   return (
-    <form onSubmit={handleSubmit}>
+    <div>
       <input
-        type="email"
+        type="text"
         id="emailAddress"
         className="peer-rounded-[1px] mb-4 w-full max-w-md rounded-lg border border-transparent border-t-transparent bg-[#D9D9D9] px-3 py-2.5 font-sans text-sm font-normal text-[#043028] outline outline-0 transition-all placeholder-shown:border-transparent focus:border-[#D9D9D9] focus:border-transparent focus:border-t-transparent focus:outline-0 disabled:border-0"
         placeholder="Enter your email here"
         value={email}
         onChange={handleEmailChange}
-        required
       />
       <br />
       <button
         type="button"
-        className="text-cabin inline-block w-full max-w-md cursor-pointer rounded-lg bg-[#82a38a] px-4 py-3 text-center text-lg font-semibold text-white transition duration-200 ease-in-out hover:bg-[#57755e]"
-        disabled={isLoading} // Disable the button while loading
+        className="text-cabin inline-block w-full max-w-md cursor-pointer rounded-lg bg-[#82A38A] px-4 py-3 text-center text-lg font-semibold text-white transition duration-200 ease-in-out hover:bg-[#57755E]"
+        disabled={isPending} // Disable the button while loading
+        onClick={handleSubmit}
       >
-        {isLoading ? <Spinner /> : 'Sign Up'}
+        {isPending ? <Spinner /> : 'Sign Up'}
       </button>
-      {successMessage && (
-        <p className="mt-4 text-center text-lg font-semibold text-customDarkGreen">
-          {successMessage}
-        </p>
+      {message && (
+        <p className="mt-4 text-center text-lg font-semibold text-customDarkGreen">{message}</p>
       )}
-    </form>
+    </div>
   );
 };
 
