bab91b952782ca1b31c3b0c2056ecd34c15b8380
donaldliu1 <donaldliu1313@gmail.com>
2024-05-23 14:19:22 +0100

    changed so mobile is slider

--- components/shop-carousel.tsx
+++ components/shop-carousel.tsx
@@ -22,32 +22,34 @@ export async function ShopCarousel({
 
   return (
     <div className="mx-none mb-4 border-b px-2 pb-2 md:px-0">
-      <p className="relative  text-[34px] text-black max-md:text-center md:pl-[24px]">{title}</p>
-      <p className="relative  text-[16px] text-black max-md:text-center md:pl-[24px]">{subtitle}</p>
-      <ul className="flex flex-wrap gap-2 pl-[24px] max-[500px]:justify-center">
-        {carouselProducts.map((product, i) => (
-          <li
-            key={`${product.handle}${i}`}
-            className="aspect-square h-full w-[210px] animate-fadeIn transition-opacity"
-          >
-            <Link href={`/product/${product.handle}`} className="relative h-full w-full">
-              <GridTileImage
-                alt={product.title}
-                src={product.featuredImage?.url}
-                fill
-                sizes="(min-width: 1024px) 25vw, (min-width: 768px) 33vw, 50vw"
-                className="object-cover object-center"
-              />
-              <p className="text-[20px]">{product.title}</p>
-              <Price
-                className="text-[16px] font-normal text-[#454545]"
-                amount={product.priceRange.maxVariantPrice.amount}
-                currencyCode={'GBP'}
-              />
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <p className="relative text-[34px] text-black max-md:text-center md:pl-[24px]">{title}</p>
+      <p className="relative text-[16px] text-black max-md:text-center md:pl-[24px]">{subtitle}</p>
+      <div className="w-full max-md:flex max-md:overflow-x-auto">
+        <ul className="flex gap-2 pl-[24px] md:flex-wrap">
+          {carouselProducts.map((product, i) => (
+            <li
+              key={`${product.handle}${i}`}
+              className="aspect-square h-full w-[210px] animate-fadeIn transition-opacity"
+            >
+              <Link href={`/product/${product.handle}`} className="relative h-full w-full">
+                <GridTileImage
+                  alt={product.title}
+                  src={product.featuredImage?.url}
+                  fill
+                  sizes="(min-width: 1024px) 25vw, (min-width: 768px) 33vw, 50vw"
+                  className="object-cover object-center"
+                />
+                <p className="text-[20px]">{product.title}</p>
+                <Price
+                  className="text-[16px] font-normal text-[#454545]"
+                  amount={product.priceRange.maxVariantPrice.amount}
+                  currencyCode={'GBP'}
+                />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
