95689443b5d42a1bdc83fdc70e92431181823eec
Oscar Oneim <52040766+oscaroneim@users.noreply.github.com>
2024-05-23 16:57:58 +0100

    added some styling on the blog

--- components/cart/close-cart.tsx
+++ components/cart/close-cart.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 
 export default function CloseCart({ className }: { className?: string }) {
   return (
-    <div className="relative flex h-11 w-11 items-center justify-center rounded-md border bg-customGreen text-white transition-colors">
+    <div className="relative flex h-11 w-11 items-center justify-center rounded-md bg-customGreen text-white transition-colors">
       <XMarkIcon className={clsx('h-6 transition-all ease-in-out hover:scale-110 ', className)} />
     </div>
   );
