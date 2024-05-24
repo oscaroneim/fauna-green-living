9e64126b5535636d28044be4bc684d4c734133da
Oscar Oneim <52040766+oscaroneim@users.noreply.github.com>
2024-05-24 18:21:45 +0100

    added head tags to each page for SEO

--- components/product/product-description.tsx
+++ components/product/product-description.tsx
@@ -1,19 +1,17 @@
 import { AddToCart } from 'components/cart/add-to-cart';
 import Price from 'components/price';
 import Prose from 'components/prose';
-import { getCart } from 'lib/shopify';
 import { Product } from 'lib/shopify/types';
-import { cookies } from 'next/headers';
 import { Suspense } from 'react';
 import { VariantSelector } from './variant-selector';
 
 export async function ProductDescription({ product }: { product: Product }) {
-  const cartId = cookies().get('cartId')?.value;
-  let cart;
+  // const cartId = cookies().get('cartId')?.value;
+  // let cart;
 
-  if (cartId) {
-    cart = await getCart(cartId);
-  }
+  // if (cartId) {
+  //   cart = await getCart(cartId);
+  // }
 
   return (
     <>
@@ -27,11 +25,6 @@ export async function ProductDescription({ product }: { product: Product }) {
         </div>
         <Suspense fallback={null}>
           <AddToCart variants={product.variants} availableForSale={product.availableForSale} />
-          {/* <BuyNow
-            variants={product.variants}
-            availableForSale={product.availableForSale}
-            cart={cart}
-          /> */}
         </Suspense>
       </div>
       <Suspense fallback={null}>
