40b239693a2b1b40d784bea641231b54532db0e1
donaldliu1 <donaldliu1313@gmail.com>
2024-05-25 21:58:40 +0100

    removed shop carousel, fixed mobile sizing

--- components/grid/index.tsx
+++ components/grid/index.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx';
 function Grid(props: React.ComponentProps<'ul'>) {
   return (
     <div className="h-full w-full max-md:flex max-md:overflow-x-auto">
-      <ul {...props} className={clsx('flex gap-2 pl-[24px] md:flex-wrap', props.className)}>
+      <ul {...props} className={clsx('flex h-full gap-2 pl-[24px] md:flex-wrap', props.className)}>
         {props.children}
       </ul>
     </div>
