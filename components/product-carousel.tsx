1cee134796aa23262db2e44ba3650de96cb62359
donaldliu1 <donaldliu1313@gmail.com>
2024-05-26 12:55:36 +0100

    homepage product mobile fix 2

--- components/product-carousel.tsx
+++ components/product-carousel.tsx
@@ -13,9 +13,9 @@ export async function Carousel() {
   const carouselProducts = [...products, ...products, ...products];
 
   return (
-    <div className="mx-none h-[410px] bg-[#677B6A] px-2 md:px-0">
+    <div className="mx-none h-[430px] bg-[#677B6A] px-2 md:px-0">
       <p className="relative pb-4 pt-2 text-center text-5xl text-white">Featured Products</p>
-      <div className=" h-full w-full overflow-x-auto pt-1 no-scrollbar">
+      <div className=" flex h-full w-full gap-4 overflow-x-auto overflow-y-hidden pt-1 no-scrollbar">
         <ul className="flex gap-[32px] md:justify-between md:px-6">
           {carouselProducts.slice(0, 5).map((product, i) => (
             <div className="h-[290px]">
