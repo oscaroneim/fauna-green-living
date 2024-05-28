421be827e08d713a8e7a360fcf310bb990b4823c
AparnaAbbina <149721457+AparnaAbbina@users.noreply.github.com>
2024-05-28 11:21:18 +0100

    Reviews added to Homepage and Wholesale page

--- components/reviews.tsx
+++ components/reviews.tsx
@@ -1,3 +1,6 @@
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export async function Reviews() {
   const renderStars = (rating: number) => {
     const fullStars = Math.floor(rating);
@@ -6,10 +9,10 @@ export async function Reviews() {
     return (
       <>
         {[...Array(fullStars)].map((_, i) => (
-          <FontAwesomeIcon key={i} icon={faStar} className="text-yellow-600" />
+          <FontAwesomeIcon key={i} icon={faStar} className="text-m text-yellow-600" />
         ))}
         {[...Array(halfStars)].map((_, i) => (
-          <FontAwesomeIcon key={i + fullStars} icon={faStar} className="text-yellow-600" />
+          <FontAwesomeIcon key={i + fullStars} icon={faStar} className="text-m text-yellow-600" />
         ))}
         {[...Array(emptyStars)].map((_, i) => (
           <FontAwesomeIcon
@@ -26,9 +29,10 @@ export async function Reviews() {
     <div className="flex space-x-12 overflow-x-auto pb-2 pt-2 no-scrollbar md:p-2 md:px-6">
       <div className="relative flex h-[180px] w-[450px] flex-col items-center justify-center rounded-lg">
         <div className="relative left-[5px] top-[0px] z-[2px] h-[95%] w-[420px] overflow-hidden rounded-[10px] border border-customGreen bg-white bg-opacity-[95%] pt-[10px] shadow-sm outline-[2px] outline-white backdrop-blur-[24px]">
-          <p className="pb-2 pl-3 text-left font-bold">
-            Mennell, Owner Welch Bakes {renderStars(5)}{' '}
-          </p>
+          <div className="flex justify-between pr-2">
+            <p className="pb-2 pl-3 text-left font-bold">Mennell, Owner Welch Bakes</p>
+            <p className="flex h-10 w-12">{renderStars(5)}</p>
+          </div>
           <p className="pl-3 text-left">
             “Working with Kim couldn't be easier. First, we have a great handmade product that
             improves the life and freshness of our sourdough! Her attention to detail to bring our
@@ -39,7 +43,10 @@ export async function Reviews() {
       </div>
       <div className="relative flex h-[180px] w-[450px]  flex-col items-center justify-center rounded-lg">
         <div className="relative left-[5px] top-[0px] z-[2px] h-[95%] w-[420px] overflow-hidden rounded-[10px] border border-customGreen bg-white bg-opacity-[95%] pt-[10px] shadow-sm outline-[2px] outline-white backdrop-blur-[24px]">
-          <p className="pb-2 pl-3 text-left font-bold">Jenni Griffiths {renderStars(5)}</p>
+          <div className="flex justify-between pr-2">
+            <p className="pb-2 pl-3 text-left font-bold">Jenni Griffiths</p>
+            <p className="flex h-10 w-12">{renderStars(5)}</p>
+          </div>
           <p className="pl-3 text-left">
             “I really liked the range of fabric choices for these beeswax wraps. They look great and
             do the job, and were delivered within the time scale promised. Would definitely
@@ -49,7 +56,10 @@ export async function Reviews() {
       </div>
       <div className="relative flex h-[180px] w-[450px]  flex-col items-center justify-center rounded-lg">
         <div className="relative left-[5px] top-[0px] z-[2px] h-[95%] w-[420px] overflow-hidden rounded-[10px] border border-customGreen bg-white bg-opacity-[95%] pt-[10px] shadow-sm outline-[2px] outline-white backdrop-blur-[24px]">
-          <p className="pb-2 pl-3 text-left font-bold">Louise Chambers {renderStars(5)}</p>
+          <div className="flex justify-between pr-2">
+            <p className="pb-2 pl-3 text-left font-bold">Louise Chambers</p>
+            <p className="flex h-10 w-12">{renderStars(5)}</p>
+          </div>
           <p className="pl-3 text-left">
             “Apart from the obvious environmental benefit these are great for my mum who has really
             bad arthritis in her hands and struggles to use cling film”
@@ -58,7 +68,10 @@ export async function Reviews() {
       </div>
       <div className="relative flex h-[180px] w-[450px]  flex-col items-center justify-center rounded-lg">
         <div className="relative left-[5px] top-[0px] z-[2px] h-[95%] w-[420px] overflow-hidden rounded-[10px] border border-customGreen bg-white bg-opacity-[95%] pt-[10px] shadow-sm outline-[2px] outline-white backdrop-blur-[24px]">
-          <p className="pb-2 pl-3 text-left font-bold">Malissa Denney {renderStars(4)}</p>
+          <div className="flex justify-between pr-2">
+            <p className="pb-2 pl-3 text-left font-bold">Malissa Denney</p>
+            <p className="flex h-10 w-12">{renderStars(5)}</p>
+          </div>
           <p className="pl-3 text-left">
             “Great quality, have been using them for my lunches and the have kept my sandwiches
             really well!”
