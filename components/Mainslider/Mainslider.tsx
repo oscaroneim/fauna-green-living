97a3f63e855b2edf68cf50f61952cd6928c36974
Oscar Oneim <52040766+oscaroneim@users.noreply.github.com>
2024-05-29 14:46:39 +0100

    tweaking title and button placement

--- components/Mainslider/Mainslider.tsx
+++ components/Mainslider/Mainslider.tsx
@@ -138,11 +138,11 @@ export default function Mainslider() {
                     <div className="shadow-xs justify-center bg-transparent bg-opacity-0">
                       {/*-----------------------------Styling for Eco friendly slide starts here------------------------------- */}
                       {index === 1 && (
-                        <div className={`flex flex-col gap-1 md:mt-[150px] md:gap-5 lg:ml-6`}>
-                          <h2 className="m-4 text-center text-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:m-0  md:text-left  md:text-4xl lg:text-5xl">
+                        <div className={`flex flex-col gap-1 md:ml-14 md:mt-20 md:gap-5 lg:ml-20`}>
+                          <h2 className="m-4 text-center text-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:m-0 md:ml-8  md:text-left  md:text-4xl lg:text-5xl">
                             Plastic Free <br /> Zero Waste <br /> Locally Sourced
                           </h2>
-                          <button className="order-1 cursor-pointer rounded-md border-none bg-customDarkGreen px-4 py-2 text-base font-bold text-white md:order-2 md:ml-auto md:mr-0 md:h-[62px] md:w-[310px] md:text-base lg:ml-[4px]">
+                          <button className="order-1 cursor-pointer rounded-md border-none bg-customDarkGreen px-4 py-2 text-base font-bold text-white md:order-2 md:ml-[35px] md:mr-0 md:h-[62px] md:w-[300px] md:text-base">
                             <Link href={element.buttonLink}>{element.buttonText}</Link>
                           </button>
                         </div>
@@ -160,7 +160,7 @@ export default function Mainslider() {
                       )}
                       {index !== 1 && index !== 2 && (
                         <div
-                          className={`flex flex-col gap-2 md:gap-5 ${index === 0 && isMobile ? 'items-center' : ''} ${index === 3 ? ' -mt-40 items-start pl-8 md:-mt-48 lg:mt-0' : ''} `}
+                          className={`flex flex-col gap-2 md:gap-5 ${index === 0 && isMobile ? 'items-center' : ''} ${index === 0 && isLargeScreen ? '-mt-48 ml-10' : ''} ${index === 3 && isLargeScreen ? 'ml-8' : ''} ${index === 3 ? ' -mt-40 items-start pl-8 md:-mt-48 lg:mt-0' : ''} `}
                         >
                           <h2
                             className={` text-left text-3xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:text-5xl ${index === 0 && isMobile ? 'm-[65px] h-[20px] w-auto text-center' : ''} ${index === 3 ? 'w-[80%]' : ''}`}
