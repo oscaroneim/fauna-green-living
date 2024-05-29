d85ddcabfbd7ed1c887ea3dd23b7153b69e38388
Oscar Oneim <52040766+oscaroneim@users.noreply.github.com>
2024-05-29 13:34:42 +0100

    completed carousel

--- components/Mainslider/Mainslider.tsx
+++ components/Mainslider/Mainslider.tsx
@@ -17,6 +17,7 @@ export default function Mainslider() {
   const plugin = React.useRef(Autoplay({ delay: 6000, stopOnInteraction: true }));
   const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
   const isIpad = useMediaQuery({ query: '(max-width: 1024px)' });
+  const isLargeScreen = useMediaQuery({ query: '(min-width: 1025px)' });
 
   const trial = [
     {
@@ -130,25 +131,25 @@ export default function Mainslider() {
                   />
 
                   <div
-                    className={`absolute flex items-center justify-center text-white md:inset-0 ${index === 0 ? 'md:mt-[160px]' : ''} md:items-start md:justify-start md:p-24 ${index === 4 ? 'md:mt-[25px]' : ''}`}
+                    className={`absolute flex h-full w-full items-center justify-center text-white md:inset-0 ${index === 0 ? 'md:mt-[160px]' : ''} md:justify-start md:p-12 ${index === 4 ? 'md:mt-[25px]' : ''}`}
                   >
                     {/*-----------------------------------------Title and button continer starts here------------------------------------------*/}
 
                     <div className="shadow-xs justify-center bg-transparent bg-opacity-0">
                       {/*-----------------------------Styling for Eco friendly slide starts here------------------------------- */}
                       {index === 1 && (
-                        <div className="flex flex-col gap-4 md:mt-[150px]">
+                        <div className={`flex flex-col gap-1 md:mt-[150px] md:gap-5`}>
                           <h2 className="m-4 text-center text-2xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:m-0  md:text-left  md:text-4xl lg:text-5xl">
                             Plastic Free <br /> Zero Waste <br /> Locally Sourced
                           </h2>
-                          <button className="order-1 ml-8 cursor-pointer rounded-md border-none bg-customDarkGreen px-4 py-2 text-base font-bold text-white md:order-2 md:ml-auto md:mr-0 md:h-[62px] md:w-[310px] md:text-base">
+                          <button className="order-1 cursor-pointer rounded-md border-none bg-customDarkGreen px-4 py-2 text-base font-bold text-white md:order-2 md:ml-auto md:mr-0 md:h-[62px] md:w-[310px] md:text-base lg:ml-[4px]">
                             <Link href={element.buttonLink}>{element.buttonText}</Link>
                           </button>
                         </div>
                       )}
                       {/*-----------------------------Styling for Summer Collection slide starts here------------------------------- */}
                       {index === 2 && (
-                        <div className="mt-[300px] flex flex-col items-center gap-4 md:mt-[160px] md:items-start lg:ml-28">
+                        <div className="mt-[300px] flex flex-col items-center gap-5 md:mt-[50px] md:items-start lg:ml-40">
                           <h2 className="w-[70%] text-center text-3xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:text-left md:text-5xl md:text-customDarkGreen md:drop-shadow-[0_1.2px_1.2px_rgba(255,255,255)] lg:text-6xl">
                             {element.title}
                           </h2>
@@ -159,15 +160,15 @@ export default function Mainslider() {
                       )}
                       {index !== 1 && index !== 2 && (
                         <div
-                          className={`flex flex-col items-center ${index === 0 && isMobile ? 'ml-[20px] mt-[250px] ' : ''} ${index === 3 && isMobile ? 'mt-[128px] items-start' : ''}`}
+                          className={`flex flex-col gap-2 md:gap-5 ${index === 0 && isMobile ? 'items-center' : ''} ${index === 3 ? ' -mt-40 items-start pl-8 md:-mt-48 lg:mt-0' : ''} `}
                         >
                           <h2
-                            className={` text-left text-3xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:w-[80%] md:text-left md:text-5xl ${index === 3 && isMobile ? 'w-[80%]' : ''} ${index === 0 && isMobile ? 'm-[65px] h-[20px] w-auto text-center' : ''}`}
+                            className={` text-left text-3xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:text-5xl ${index === 0 && isMobile ? 'm-[65px] h-[20px] w-auto text-center' : ''} ${index === 3 ? 'w-[80%]' : ''}`}
                           >
                             {element.title}
                           </h2>
                           <div
-                            className={`flex flex-col items-start ${index === 3 && isMobile ? 'ml-[70px] w-full' : ''}`}
+                            className={`flex flex-col items-start ${index === 3 && isMobile ? 'w-full' : ''} ${isIpad && index === 3 ? 'w-full' : ''}`}
                           >
                             <button
                               className={` order-1 cursor-pointer rounded-md border-none bg-customDarkGreen px-4 py-2 text-base font-bold text-white md:order-2 md:h-[62px] md:w-[310px] md:text-base ${index === 3 && isMobile ? 'mt-1' : ''} ${index === 0 && isMobile ? 'ml-[8px]' : ''}`}
